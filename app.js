794eb9595ff2c0f7205ffed5949681bd579df324
htslearn <htslearntechnologies@gmail.com>
2024-12-10 22:03:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+require('dotenv').config();
+
+
+const app = express();
+
+const PORT = process.env.PORT
+const MONGO_URI = process.env.MONGO_URI 
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening on port ${PORT}`);
+});
---
+++ controller.js
@@ -0,0 +1,11 @@
+
+
+exports.register = async (req, res) =>{
+    try {
+
+        // http request and response
+        
+    } catch (error) {
+        
+    }
+}
