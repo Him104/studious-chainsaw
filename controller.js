a38fde0219f4fc490b42512ff8b42f200bce40fe
htslearn <htslearntechnologies@gmail.com>
2024-12-16 22:00:21 +0530

    query params and path params

--- controller.js
+++ controller.js
@@ -14,17 +14,37 @@ exports.register = async (req, res) => {
     res
       .status(201)
       .send({ message: "User created successfully", data: createUser });
-  } catch (error) {}
+  } catch (error) { }
 };
 
 
 exports.getUser = async (req, res) => {
   try {
-  const {userId} = req.params
-  console.log(userId);
+    const { userId } = req.params
+    console.log(userId);
 
 
     const findUser = await userModel.findById(userId);
+
+    console.log(findUser)
+    res
+      .status(200)
+      .send({ message: "user data fetched successfully", data: findUser });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+exports.getUsersByName = async (req, res) => {
+  try {
+    const { name } = req.query
+
+
+if(!name ){
+  return res.status(400).send({message:"name and email is required"})
+}
+    const findUser = await userModel.findOne({ name: name }).lean();
 
     console.log(findUser)
     res
--- route.js
+++ route.js
@@ -3,5 +3,6 @@ const controller = require("./controller")
 const router = express.Router();
 router.post('/register', controller.register )
 router.get('/get-user/:userId', controller.getUser)
+router.get('/get-users-by-name', controller.getUsersByName)
 
 module.exports = router;
