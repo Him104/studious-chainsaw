8a99917deb006c00a82f65337f6fdb31663fa89b
htslearn <htslearntechnologies@gmail.com>
2024-12-17 22:05:46 +0530

    update-api query param and path param and get api for all users

--- controller.js
+++ controller.js
@@ -41,10 +41,14 @@ exports.getUsersByName = async (req, res) => {
     const { name } = req.query
 
 
-if(!name ){
-  return res.status(400).send({message:"name and email is required"})
-}
+    if(!name){
+      return res.status(400).send({message: "Name is required"})
+    }
     const findUser = await userModel.findOne({ name: name }).lean();
+
+    if(!findUser){
+      return res.status(404).send({message:"user not found"})
+    }
 
     console.log(findUser)
     res
@@ -54,3 +58,83 @@ if(!name ){
     console.log(error);
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+
+  try {
+    
+
+  const findAllUsers = await userModel.find();
+
+  return res.status(200).send({message: "all users fetched successfully", data: findAllUsers})
+  
+
+} catch (error) {
+
+  res.status(500).send({message: "Internal server error", error})
+    
+}
+}
+
+exports.updateUser = async (req, res) => {
+try {
+  const {userId} = req.params
+  const { name, email, password } = req.body;
+
+  if(!name && !email && !password){
+    return res.status(400).send({message: "Name, email and password are required"})
+  }
+
+const userUpdate = await userModel.findByIdAndUpdate(userId,  { name, email, password }, {new: true})
+
+if(!userUpdate){
+  return res.status(404).send({message: "user not found"})
+}
+
+return res.status(201).send({message: "user data updated successfully", data: userUpdate}) 
+
+} catch (error) {
+
+  res.status(500).send({message: "Internal server error", error})
+  
+}
+
+}
+
+
+exports.updateUserByName = async (req, res) => {
+  try {
+    const {name, email} = req.query
+    const updatedData= req.body;
+  
+    if(!name && !email){
+      return res.status(400).send({message: "Name and email are required"})
+    }
+  
+  const updatedUser = await userModel.findOneAndUpdate({$or: [{name: name}, {email: email}]},
+     {$set: updatedData},
+      {new: true})
+  
+  if(!updatedUser){
+    return res.status(404).send({message: "user not found"})
+  }
+  
+  return res.status(201).send({message: "user data updated successfully", data: updatedUser}) 
+  
+  } catch (error) {
+
+    console.log(error)
+  
+  
+   return  res.status(500).send({message: "Internal server error", error})
+    
+  }
+  
+  }
+  
+  
+  
+  
+
+
+
--- route.js
+++ route.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.post('/register', controller.register )
 router.get('/get-user/:userId', controller.getUser)
 router.get('/get-users-by-name', controller.getUsersByName)
-
+router.get('/get-allUsers', controller.getAllUsers)
+router.put('/update-user/:userId', controller.updateUser)
+router.put('/update-user-by-name', controller.updateUserByName)
 module.exports = router;
